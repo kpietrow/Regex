8e5ee5eb2e396818e99f736ab154adbf1a64502d
Kevin Pietrow <kpietrow@gmail.com>
2012-11-23 00:13:30 -0500

    First working version. Error in previous versions was that lowercase was being checked for, while words.txt is completely uppercase. This version returns all of the words in the text file.

--- RegexWork.py
+++ RegexWork.py
@@ -18,13 +18,7 @@ def open_file(fileName):
 def main():
     # open file
     text = open_file('/Users/User/Documents/Python 2.7.3/Regex/words.txt')
-    tester = re.compile(r"[a-z]+")
-    results = tester.match(text)
-    print "____________"
-    print text
-    print "_____________"
-    test = tester.findall("hello there")
-    print test
+    results = re.findall(r"[A-Z]+", text)
     print results
     return
 
