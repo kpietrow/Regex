04c55d520da104551c601c2d67cb963a7079fa92
Kevin Pietrow <kpietrow@gmail.com>
2012-11-25 02:38:51 -0500

    Added some basic menu functionality to the script. Will now ask users if they want 'r' in front, and for the regular expression to be searched. Will add more menu options going forward, and will work to save all results in a linked list.

--- thevergeSearching.py
+++ thevergeSearching.py
@@ -20,14 +20,37 @@ def search_file(searchText, text):
     """Performs RegEx() with user's input"""
     return re.findall(searchText, text)
 
+def query_user():
+    """Find user's search parameters"""
+    userParams = []
+    # ask if user wants 'raw' mode for regular expressions
+    userParams.append((raw_input("Would you like to add 'r' in front of the expression? (y/n):")).lower())
+    # ask user for regular expression to be searched
+    userParams.append(raw_input("Please enter the Regular Expression to be searched (outside quotation marks will be added automatically):"))
+    return userParams                     
+    
+
         
 
 def main():
+    xFactor = True
+    allSearches = []
     # open file
     text = input_file('/Users/User/Documents/Python 2.7.3/Regex/TheVerge.txt')
-    userInput = raw_input("What Regular Expression do you want searched? Please enter any and all parameters (r will be added automatically, as wil standard quotation marks):")
-    output = search_file(userInput, "r"+text)
-    print output
+    while xFactor:
+        # get user's search parameters
+        userInput = query_user()
+
+        # perform regex search based on user's specifications
+        if userInput[0] == "y":
+            output = search_file(userInput[1], "r"+text)
+        elif userInput[0] == "n":
+            output = search_file(userInput[1], text)
+            
+        print output
+        userContinue = (raw_input("Would you like to input another search string? (y/n):")).lower()
+        if userContinue == "n":
+            return
     return
 
 main()
