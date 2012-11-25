d86f5611171ba4e4cc69a7cd47b29b2da48b8e44
Kevin Pietrow <kpietrow@gmail.com>
2012-11-25 13:36:23 -0500

    trying to implement global 2D list for search results

--- thevergeSearching.py
+++ thevergeSearching.py
@@ -4,8 +4,13 @@
 import re
 import sys
 
-allSearches = []
-totalSearch = 0
+# set up 2D list
+allSearchResults = [None] * 1
+for i in range(3):
+    allSearchResults[i] = [None] * 4
+
+# set up global counter for amount of searches
+totalSearches = 0
 
 # open text file
 def input_file(fileName):
@@ -21,23 +26,33 @@ def input_file(fileName):
 
 # Necessary code
 
-def search_file(searchText, text):
-    """Performs RegEx() with user's input"""
-    totalSearch += 1
-    return re.findall(searchText, text)
-
-# Necessary, but need to decide where it will be incorporated.
-
-def query_user():
-    """Find user's search parameters"""
+def new_search_regex(textFile):
+    """Finds users search parameters, then performs RegEx() with user's input"""
     userParams = []
     # ask if user wants 'raw' mode for regular expressions
     userParams.append((raw_input("Would you like to add 'r' in front of the expression? (y/n): ")).lower())
     # ask user for regular expression to be searched
     userParams.append(raw_input("Please enter the Regular Expression to be searched (outside quotation marks will be added automatically): "))
-    searchInformation = regex_search(userParams)
-    
-    return searchInformation
+    totalSearches += 1
+
+    # perform regex search
+    if userParams[0] == "y":
+        foundRegex = re.findall('r'+userParams[1], text)
+    if userParams[0] == "n":
+        foundRegex = re.findall('r'+userParams[1], text)
+        
+    print foundRegex
+
+    # store result and search parameters in global 2D list
+    if totalSearches == 1:
+        allSearchResults[0] = [foundRegex, userParams[0], userParams[1], totalSearches]
+    else:
+        allSearchResults[totalSearches - 1] = [foundRegex, userParams[0], userParams[1], totalSearches]
+
+    return
+
+# Necessary, but need to decide where it will be incorporated.
+
 
 # Correct that this code is standalone, but need to decide if the regex should just be
 # searched from within here.
@@ -45,18 +60,9 @@ def query_user():
 # Also, I feel like there need to be more questions to the user, but can't think of them
 # at this time.
 
-def regex_search(information):
-    """Perform a regex search on the text file"""
-    
-# Version of code already exists in search_file().
-#
-# Need to decide if this function will be standalone, part of query_user(), or part of
-# main(). Could be good as standalone as error checking could be done here.
-#
-# If it is standalone though, need to fully figure out how new listing will be carried
-# back to main(), or if it will be displayed here.
 
-def main_menu():
+
+def main_menu(textFile):
     """Main menu selection for user options in the script"""
     xFactor = True
     while xFactor:
@@ -67,9 +73,9 @@ def main_menu():
         
         # call different functions depending on user input
         if userInstr == "1":
-            inputInfo = query_user()
+            new_search_regex(textFile)
         elif userInstr == "2":
-            inputInfo = search_result()
+            search_result()
         elif userInstr == "3":
             print "Exiting program now."
             sys.exit(0)
@@ -78,7 +84,7 @@ def main_menu():
             continue
         
         # return list object with necessary information
-        return inputInfo
+        return
 
 # Need to decide if this will be the central hub for the program, or the main() function
 # As of now, the code has multiple different return values from functions all funneling
@@ -91,7 +97,7 @@ def main_menu():
 def search_result():
     """Search for prior result"""
     searchNumber = raw_input("What was the search number of the result that you are interested in?: ")
-    return allSearches[searchNumber - 1]
+    return allSearchResults[searchNumber - 1]
 
 # This function is necessary. Need to decide how it returns the searched for result,
 # and need to build error checking into the search
@@ -105,18 +111,7 @@ def main():
     text = input_file('/Users/User/Documents/Python 2.7.3/Regex/TheVerge.txt')
     while xFactor:
         # get user's search parameters
-        userInput = main_menu()
-
-        # perform regex search based on user's specifications
-        if userInput[0] == "y":
-            output = search_file(userInput[1], "r"+text)
-        elif userInput[0] == "n":
-            output = search_file(userInput[1], text)
-            
-        print output
-        userContinue = (raw_input("Would you like to input another search string? (y/n): ")).lower()
-        if userContinue == "n":
-            return
+        main_menu(text)
     return
 
 # Bit of a confusing mess at the moment. Need to simplify this function, and decide where
