086e1d8123d7786c67ce0683e713cd2f659220fa
Kevin Pietrow <kpietrow@gmail.com>
2012-11-25 14:07:12 -0500

    Code now has basic functionality on searching new regex string, and looking up old one.

--- thevergeSearching.py
+++ thevergeSearching.py
@@ -5,8 +5,8 @@ import re
 import sys
 
 # set up 2D list
-allSearchResults = [None] * 1
-for i in range(3):
+allSearchResults = [None] * 20
+for i in range(1):
     allSearchResults[i] = [None] * 4
 
 # set up global counter for amount of searches
@@ -29,6 +29,9 @@ def input_file(fileName):
 def new_search_regex(textFile):
     """Finds users search parameters, then performs RegEx() with user's input"""
     userParams = []
+    global totalSearches
+    global allSearchResults
+    
     # ask if user wants 'raw' mode for regular expressions
     userParams.append((raw_input("Would you like to add 'r' in front of the expression? (y/n): ")).lower())
     # ask user for regular expression to be searched
@@ -37,35 +40,40 @@ def new_search_regex(textFile):
 
     # perform regex search
     if userParams[0] == "y":
-        foundRegex = re.findall('r'+userParams[1], text)
-    if userParams[0] == "n":
-        foundRegex = re.findall('r'+userParams[1], text)
-        
-    print foundRegex
-
-    # store result and search parameters in global 2D list
-    if totalSearches == 1:
-        allSearchResults[0] = [foundRegex, userParams[0], userParams[1], totalSearches]
+        foundRegex = re.findall('r'+userParams[1], textFile)
+    elif userParams[0] == "n":
+        foundRegex = re.findall(userParams[1], textFile)
     else:
-        allSearchResults[totalSearches - 1] = [foundRegex, userParams[0], userParams[1], totalSearches]
+        foundRegex = re.findall(userParams[1], textFile)
 
+    if foundRegex != None:
+        print foundRegex
+
+        # store result and search parameters in global 2D list
+        if totalSearches == 1:
+            allSearchResults[0] = [foundRegex, userParams[0], userParams[1], totalSearches]
+            print "Result has been stored. Its search number is 1."
+        elif totalSearches == 21:
+            print "I am sorry, but you have too many saved results to store another value."
+        else:
+            allSearchResults[totalSearches - 1] = [foundRegex, userParams[0], userParams[1], totalSearches]
+            print "Result has been stored. Its search number is" + str(totalSearches)
+
+    else:
+        print "Search did not have any results."
+    
     return
 
-# Necessary, but need to decide where it will be incorporated.
-
-
-# Correct that this code is standalone, but need to decide if the regex should just be
-# searched from within here.
-#
-# Also, I feel like there need to be more questions to the user, but can't think of them
-# at this time.
+# One of the menu options. Will handle both querying user and searching for regex
 
 
 
 def main_menu(textFile):
     """Main menu selection for user options in the script"""
     xFactor = True
+    
     while xFactor:
+        print ""
         print "1- Enter new search string"
         print "2- Access prior search result"
         print "3- Exit program"
@@ -73,31 +81,34 @@ def main_menu(textFile):
         
         # call different functions depending on user input
         if userInstr == "1":
+            print ""
             new_search_regex(textFile)
         elif userInstr == "2":
-            search_result()
+            print ""
+            search_past_result()
         elif userInstr == "3":
             print "Exiting program now."
             sys.exit(0)
         else:
             print "Not a valid response, please try again."
+            print ""
             continue
-        
-        # return list object with necessary information
-        return
+    
 
-# Need to decide if this will be the central hub for the program, or the main() function
-# As of now, the code has multiple different return values from functions all funneling
-# through this one function
+# This will be the central hub for the program.
+# Will control when user decides to exit the program.
 
 
 
 
 
-def search_result():
+def search_past_result():
     """Search for prior result"""
+    global allSearchResults
+    
     searchNumber = raw_input("What was the search number of the result that you are interested in?: ")
-    return allSearchResults[searchNumber - 1]
+    allSearchResults[int(searchNumber) - 1]
+    return
 
 # This function is necessary. Need to decide how it returns the searched for result,
 # and need to build error checking into the search
