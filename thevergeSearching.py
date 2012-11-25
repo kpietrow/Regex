a353988c343d98068cb30060b2f36f2648b2e40e
Kevin Pietrow <kpietrow@gmail.com>
2012-11-25 03:21:56 -0500

    Added a lot of comments detailing work that needs to be done.

--- thevergeSearching.py
+++ thevergeSearching.py
@@ -19,10 +19,14 @@ def input_file(fileName):
     text = rt.read()
     return text
 
+# Necessary code
+
 def search_file(searchText, text):
     """Performs RegEx() with user's input"""
     totalSearch += 1
     return re.findall(searchText, text)
+
+# Necessary, but need to decide where it will be incorporated.
 
 def query_user():
     """Find user's search parameters"""
@@ -32,10 +36,25 @@ def query_user():
     # ask user for regular expression to be searched
     userParams.append(raw_input("Please enter the Regular Expression to be searched (outside quotation marks will be added automatically): "))
     searchInformation = regex_search(userParams)
+    
     return searchInformation
+
+# Correct that this code is standalone, but need to decide if the regex should just be
+# searched from within here.
+#
+# Also, I feel like there need to be more questions to the user, but can't think of them
+# at this time.
 
 def regex_search(information):
     """Perform a regex search on the text file"""
+    
+# Version of code already exists in search_file().
+#
+# Need to decide if this function will be standalone, part of query_user(), or part of
+# main(). Could be good as standalone as error checking could be done here.
+#
+# If it is standalone though, need to fully figure out how new listing will be carried
+# back to main(), or if it will be displayed here.
 
 def main_menu():
     """Main menu selection for user options in the script"""
@@ -61,10 +80,21 @@ def main_menu():
         # return list object with necessary information
         return inputInfo
 
+# Need to decide if this will be the central hub for the program, or the main() function
+# As of now, the code has multiple different return values from functions all funneling
+# through this one function
+
+
+
+
+
 def search_result():
     """Search for prior result"""
     searchNumber = raw_input("What was the search number of the result that you are interested in?: ")
     return allSearches[searchNumber - 1]
+
+# This function is necessary. Need to decide how it returns the searched for result,
+# and need to build error checking into the search
     
 
 def main():
@@ -89,4 +119,12 @@ def main():
             return
     return
 
+# Bit of a confusing mess at the moment. Need to simplify this function, and decide where
+# the functionality contained here should go.
+
 main()
+
+
+
+# Ultimately, need to work out return sequence for values, which functions are necessary,
+# and simplify the code where I can.
