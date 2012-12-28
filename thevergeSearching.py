b1388b54abecda607694cfdf5a6ae3b761718c8f
Kevin Pietrow <kpietrow@gmail.com>
2012-12-27 23:28:37 -0500

    Performed maintenance on code. Cleaned up some output issues. Past results, determining successfull searches, and making data persistent are goals for future.

--- thevergeSearching.py
+++ thevergeSearching.py
@@ -15,46 +15,50 @@ import re
 import sys
 import os
 
-# set up 2D list
-"""
-allSearchResults = [None] * 50
-for i in range(1):
-    allSearchResults[i] = [None] * 4
-"""
-
-# Class for searches
-class Reg_Search(object):
-
-    def __init__(self, search_number, expression, result):
-        self.search_number = search_number
-        self.expression = expression
-        self.result = result
-
-    def display_search(self):
-        print "Search number: " + self.search_number + "\n\nExpression searched: " + self.expression +
-        "\n\nResult: " + self.result
+# set up global array for storing results
+allSearchResults = []
 
 # set up global counter for amount of searches
 totalSearches = 0
 
 
+# Class for searches
+class Reg_Search(object):
+
+    def __init__(self, search_number, expression, result):
+        self.search_number = search_number    # Global search number
+        self.expression = expression          # Regular expression searched
+        self.result = result                  # Result of regex() search
+
+    def display_search(self):
+        # prints out object information
+        print "\nSearch number: " + str(self.search_number) + "\nExpression searched: " + str(self.expression) + "\nResult: " + str(self.result) + "\n"
+
+
 def input_file():
     """Open file"""
-    textFile = raw_input("What file would you like to search?: ")
+    textFile = raw_input("What file would you like to search?: ")        # Query user for filename
+
+    # Attempt to open file
     while True:
         try:
-            rt = open(os.path.abspath(textFile), 'r')
+            rt = open(os.path.abspath(textFile), 'r')                    # Use os.* to search user's machine
+
+        # Catch nonexistent file error
         except IOError:
             print "Failure to locate file."
-            textFile = raw_input("Please enter a different file: ")
+            textFile = raw_input("Please enter a different file: ")      # Query for new file
             continue
+
         else:
             break
 
     text = rt.read()
+    print "Successfully loaded the file!"
     return text
 
 # Necessary code. This opens up the text file that the user requested.
+# Acceptable error checking
 
 
 def new_search_regex(textFile):
@@ -63,8 +67,7 @@ def new_search_regex(textFile):
     global allSearchResults
 
     # ask user for regular expression to be searched
-    expression = raw_input("Please enter the Regular Expression to be searched: "))
-    print expression
+    expression = raw_input("Please enter the Regular Expression to be searched: ")
     totalSearches += 1
 
     # perform regex search
@@ -72,11 +75,16 @@ def new_search_regex(textFile):
 
     # if Regex search successful
     if len(foundRegex) != 0:
+
+    # Note: This ^ does not successfully determine anything.
+    # Needs work
+
         print foundRegex
 
         # create object for result, store in global array
         reg_object = Reg_Search(totalSearches, expression, foundRegex)
-        allSearchResults[totalSearches - 1] = reg_object
+        allSearchResults.append(reg_object)
+        print "You're search number for this search is " + str(totalSearches)    # Inform user of storage location
 
     # if Regex search unsuccessful
     else:
@@ -85,7 +93,7 @@ def new_search_regex(textFile):
     return
 
 # One of the menu options. Will handle
-# both querying user and searching for regex
+# both querying user for expression, and searching for regex
 
 
 
@@ -96,19 +104,27 @@ def main_menu(textFile):
     while xFactor:
         print "\n1- Enter new search string"
         print "2- Access prior search result"
-        print "3- Exit program"
+        print "3- Display loaded file"
+        print "4- Exit program"
         userInstr = raw_input("Selection: ")
 
-        # call different functions depending on user input
+        # Call different functions depending on user input
+        # Call searcher function
         if userInstr == "1":
             print ""
             new_search_regex(textFile)
+        # Call past result function
         elif userInstr == "2":
             print ""
             search_past_result()
+        # Display text to be searched
         elif userInstr == "3":
+            print textFile + "\n"
+        # Exit program
+        elif userInstr == "4":
             print "Exiting program now."
             return
+        # If invalid response
         else:
             print "Not a valid response, please try again.\n"
             continue
@@ -136,15 +152,37 @@ def search_past_result():
         # search for search number
         if userInput == "1":
             search = int(raw_input("Please input search number: "))
-            reg_object = allSearchResults[searchNumber - 1]        # get from global array
+            try:
+                allSearchResults[search].display_search()
+            except IndexError:
+                print "Could not locate data. Please try different search criteria."
 
-        # search for search string
+        # query for search expression
         elif userInput == "2":
-            search = raw_input("Please input search string: ")
+            unsuccessful = True
+            search = raw_input("Please input expression: ")     # Query user for expression to search for
+            for item in allSearchResults:                       # Runs through global array
+                if search == item.expression:                   # If a match for object property
+                    item.display_search()                       # Display object
+                    unsuccessful = False
 
-        # search for past results
+            if unsuccessful:
+                print "Could not locate data. Please try different search criteria."
+
+        # query for search result
         elif userInput == "3":
+            unsuccessful = True
             search = raw_input("Please input search result: ")
+            for item in allSearchResults:
+                if search == item.result:
+                    item.display_search()
+                    unsuccessful = False
+
+            if unsuccessful:
+                print "Could not locate data. Please try different search criteria."
+
+        # Note: this section is broken. Must determine how to effectively
+        # search for the result criteria
 
         # exit to main menu
         elif userInput == "4":
@@ -154,15 +192,21 @@ def search_past_result():
             continue
 
     # ask user if they want to search prior results again
-        userInput = raw_input("Would you like find another prior result? (y/n): ").lower()
-        if userInput == "n":
-            return
+        while True:
+            userInput = raw_input("Would you like find another prior result? (y/n): ").lower()
+            if userInput == "n":
+                return
+            elif userInput == "y":
+                continue
+            else:
+                print "Error. Invalid response. Please try again"
 
     return
 
 
 # This function is necessary.
-# Need to decide how it returns the searched for result,
+# Call a display function in the Reg_Search class to display results
+# Could implement functionality to print result to file
 # and need to build error checking into the search
 
 # Added more search options
@@ -171,8 +215,8 @@ def search_past_result():
 def main():
     # Print initial greeting
     print "Hello, and welcome to 'Searching TheVerge'!."
-    text = input_file()
-    main_menu(text)
+    text = input_file()            # Call function to obtain search file
+    main_menu(text)                # Call the main menu function
     return
 
 # Have successfully simplified main() as much as possible
@@ -184,3 +228,7 @@ main()
 # Ultimately, need to work out return sequence for values,
 # which functions are necessary,
 # and simplify the code where I can.
+
+# Also, past search section needs work
+# Need to figure out how to keep these as persistent values
+# ^ sqlite may be the answer
